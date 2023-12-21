2e16b008b7f3c672ea198b739f4608db990ba385
Alice Birkett <alicebirkett@alice-b-mbp.home>
2023-12-21 13:28:29 +0000

    updated testing for part 3 refactor

--- src/main/kotlin/unscramble/UnscrambleGame.kt
+++ src/main/kotlin/unscramble/UnscrambleGame.kt
@@ -5,19 +5,6 @@ class UnscrambleGame(fileUtils: FileUtils) {
     val repository = fileUtils
     private val wordList: List<String> = generateWords()
 
-    fun unscramble() {
-        // val word = randomWord()
-        // val randomised = jumbleWord(word)
-        // val input = readLine()
-        // if input == word, Congratulations! you win, exitProcess()
-        // else Oh no! That's not the correct word, exitProcess()
-//        println("Welcome to Unscramble!")
-//        println("Try and unscramble these two words, you only get one shot at each!")
-//        runGame()
-//        println("Exiting program now...")
-//        kotlin.system.exitProcess(0)
-    }
-
     fun generateWords(): List<String> {
         return repository.readFromPath("./words.txt")
             .filter {
@@ -28,16 +15,6 @@ class UnscrambleGame(fileUtils: FileUtils) {
     fun randomWords(wordCount: Int): List<String> {
         return wordList.shuffled().take(wordCount)
     }
-
-//    fun runGame(words: List<String>): Unit {
-//        words.forEachIndexed {index, it ->
-//            val randomised = jumbleWord(it)
-//            println("Your jumbled word is: $randomised")
-//            print("Take a guess: ")
-//            val userInput: String = readln()
-//            parseInput(userInput, index)
-//        }
-//    }
 
     fun jumbleWords(words: List<String>): List<String> {
         return words.map {
--- src/test/kotlin/unscramble/UnscrambleGameTest.kt
+++ src/test/kotlin/unscramble/UnscrambleGameTest.kt
@@ -7,85 +7,72 @@ import com.github.stefanbirkner.systemlambda.SystemLambda.*
 
 class UnscrambleGameTest {
 
-//    @Test
-//    fun `randomWord method returns a single 5 letter word`() {
-//        val data: List<String> = listOf("hello", "cat", "goodbye")
-//        val mockRepository = Mockito.mock(FileUtils::class.java)
-//        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
-//        val underTest = UnscrambleGame(mockRepository)
-//        val expected: Int = 5
-//        val result: Int  = underTest.randomWords(1)[0].length
-//        assertEquals(expected, result)
-//    }
-//
-//    @Test
-//    fun `randomWord method returns a word from within the hard coded data`() {
-//        val data: List<String> = listOf("pound", "trice", "hired", "comma", "logic")
-//        val mockRepository = Mockito.mock(FileUtils::class.java)
-//        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
-//        val underTest = UnscrambleGame(mockRepository)
-//        val expected: Boolean = true
-//        val result: Boolean = underTest.randomWords(1)[0] in data
-//        assertEquals(expected, result)
-//    }
-//
-//    @Test
-//    fun `jumbledWord method does not return the original word`() {
-//        val data: List<String> = listOf("where")
-//        val mockRepository = Mockito.mock(FileUtils::class.java)
-//        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
-//        val underTest = UnscrambleGame(mockRepository)
-//        val unexpected: String = "where"
-//        val result: String = underTest.jumbleWord(underTest.words[0])
-//        assertNotEquals(unexpected, result)
-//    }
-//
-//    @Test
-//    fun `parseInput method sets victory to true when the words match`() {
-//        val data: List<String> = listOf("hello")
-//        val mockRepository = Mockito.mock(FileUtils::class.java)
-//        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
-//        val underTest = UnscrambleGame(mockRepository)
-//        val expected: String = "Congratulations! Your guess of hello was correct!"
-//        val result: String = tapSystemOut {
-//            underTest.parseInput("hello", 0)
-//        }.trim()
-//        assertEquals(expected, result)
-//    }
-//
-//    @Test
-//    fun `parseInput method prints appropriately for multiple guesses`() {
-//        val data: List<String> = listOf("hello", "where")
-//        val mockRepository = Mockito.mock(FileUtils::class.java)
-//        val underTest = UnscrambleGame(mockRepository)
-//        val expected1: String = "Congratulations! Your guess of hello was correct!"
-//        val expected2: String = "Oh no! Your guess of jello was incorrect!"
-//        underTest.words = data
-//        val result1: String =  tapSystemOut {
-//            underTest.parseInput("hello", 0)
-//        }.trim()
-//        val result2: String = tapSystemOut {
-//            underTest.parseInput("jello", 1)
-//        }.trim()
-//        assertEquals(expected1, result1)
-//        assertEquals(expected2, result2)
-//    }
-//
-//    @Test
-//    fun `parseInput method parses non-alphabetic data and prints appropriately`() {
-//        val data: List<String> = listOf("hello", "where")
-//        val mockRepository = Mockito.mock(FileUtils::class.java)
-//        val underTest = UnscrambleGame(mockRepository)
-//        val expected1: String = "Oh no! Your guess of 1-2=-30;'11203- was incorrect!"
-//        val expected2: String = "Oh no! Your guess of   was incorrect!"
-//        underTest.words = data
-//        val result1: String = tapSystemOut {
-//            underTest.parseInput("1-2=-30;'11203-", 0)
-//        }.trim()
-//        val result2: String = tapSystemOut {
-//            underTest.parseInput(" ", 1)
-//        }.trim()
-//        assertEquals(expected1, result1)
-//        assertEquals(expected2, result2)
-//    }
+    @Test
+    fun `randomWord method returns a single 5 letter word`() {
+        val data: List<String> = listOf("hello", "cat", "goodbye")
+        val mockRepository = Mockito.mock(FileUtils::class.java)
+        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
+        val underTest = UnscrambleGame(mockRepository)
+        val expected: Int = 5
+        val result: Int  = underTest.randomWords(1)[0].length
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `randomWord method returns a word from within the hard coded data`() {
+        val data: List<String> = listOf("pound", "trice", "hired", "comma", "logic")
+        val mockRepository = Mockito.mock(FileUtils::class.java)
+        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
+        val underTest = UnscrambleGame(mockRepository)
+        val expected: Boolean = true
+        val result: Boolean = underTest.randomWords(1)[0] in data
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `jumbledWord method does not return the original word`() {
+        val data: List<String> = listOf("where")
+        val mockRepository = Mockito.mock(FileUtils::class.java)
+        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
+        val underTest = UnscrambleGame(mockRepository)
+        val unexpected: String = "where"
+        val result: String = underTest.jumbleWords(data)[0]
+        assertNotEquals(unexpected, result)
+    }
+
+    @Test
+    fun `parseInput method sets victory to true when the words match`() {
+        val data: List<String> = listOf("hello")
+        val mockRepository = Mockito.mock(FileUtils::class.java)
+        Mockito.`when`(mockRepository.readFromPath("./words.txt")).thenReturn(data)
+        val underTest = UnscrambleGame(mockRepository)
+        val expected: Boolean = true
+        val result: Boolean = underTest.parseInput("hello", data[0])
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `parseInput method prints appropriately for multiple guesses`() {
+        val data: List<String> = listOf("hello", "where")
+        val mockRepository = Mockito.mock(FileUtils::class.java)
+        val underTest = UnscrambleGame(mockRepository)
+        val expected1: Boolean = true
+        val expected2: Boolean = false
+        val result1: Boolean = underTest.parseInput("hello", data[0])
+        val result2: Boolean = underTest.parseInput("jello", data[1])
+        assertEquals(expected1, result1)
+        assertEquals(expected2, result2)
+    }
+
+    @Test
+    fun `parseInput method parses non-alphabetic data and prints appropriately`() {
+        val data: List<String> = listOf("hello", "where")
+        val mockRepository = Mockito.mock(FileUtils::class.java)
+        val underTest = UnscrambleGame(mockRepository)
+        val expected: Boolean = false
+        val result1: Boolean = underTest.parseInput("1-2=-30;'11203-", data[0])
+        val result2: Boolean = underTest.parseInput(" ", data[1])
+        assertEquals(expected, result1)
+        assertEquals(expected, result2)
+    }
 }
